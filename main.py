e87f1fdf32dd628799346f7a5fc747fc086a1776
Husain Mithaiwala <mithaiwalahusain2173@gmail.com>
2024-08-20 15:28:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-import pickle
-
-def load_object(filename):
-    with open(filename, 'rb') as f:
-        return pickle.load(f)
-
-# Vulnerable code: Unrestricted pickle deserialization
-user_data = load_object('user_input.pkl')
+if __name__ == "__main__":
+    main()
